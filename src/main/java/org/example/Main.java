337d17f8dc300e499180d336d56748945531cfdd
Ítalo Costa <italo.iba@hotmail.com>
2025-04-18 17:08:38 -0300

    refactor: new course java

--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-    Pessoa p = new Pessoa("nome", "4545454", 78, 'm', "454578789");
-    p.apresentar();
-    }
-}
---
+++ src/test/java/br/com/italobruno/demo/CursoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italobruno.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
